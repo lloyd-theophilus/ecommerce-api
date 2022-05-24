8f7f56bc652b3b815c809c80bcd5592d4f9e460a
Lloyd Theophilus <lloydtheophilus@gmail.com>
2022-05-24 23:35:12 +0100

    added admin access token to users

--- routes/verifyToken.js
+++ routes/verifyToken.js
@@ -4,6 +4,7 @@ const jwt = require('jsonwebtoken')
 const verifyToken = (req, res, next) => { 
     const authHeader = req.headers.token;
     if (authHeader) { 
+        const token = authHeader.split(' ')[1];
         jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
             if (err) {
                 return res.status(401).json({ message: 'Invalid token' })
@@ -24,4 +25,16 @@ const verifyTokenAndAuthorization = (req, res, next) => {
     })
 }
 
-module.exports = { verifyToken, verifyTokenAndAuthorization };
+const verifyTokenAndAdmin = (req, res, next) => {
+
+    verifyToken(req, res, () => {
+        if (req.user.isAdmin) {
+            next()
+        } else {
+            res.status(401).json({ message: 'Unauthorized' })
+        }
+    })
+}
+
+
+module.exports = { verifyToken, verifyTokenAndAuthorization, verifyTokenAndAdmin };
